c5cbaad12a8ead585cf63d0ce4aee6c3eefece62
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2026-02-05 04:11:20 +0500

    add backend setup

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors(
+
+))
+
+const port = 3000;
+app.listen(port , () => {
+  console.log('Server is running on port 3000');
+}
+);
+
+
+export default app;
