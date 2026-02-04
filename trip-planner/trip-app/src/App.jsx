78c5e2bac81750a21fade7cc0347ad74fb9889e2
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2026-02-05 04:10:20 +0500

    add setup

---
+++ trip-planner/trip-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+import Dashboard from './pages/dashboard'
+
+function App() {
+
+  return (
+    
+   <div className="bg-gray-600 text-white p-4">
+    <Dashboard/>
+
+   </div>
+  )
+}
+
+export default App
